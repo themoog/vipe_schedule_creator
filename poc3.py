f937096dd2f3736b21f1695d3ce5059a9c066e2a
Neil Bernard <neilbernard@Neils-MacBook-Air.local>
2023-06-30 14:55:31 +0100

    Basic Schedule with ADS

--- poc3.py
+++ poc3.py
@@ -1,22 +1,19 @@
 import csv
 import json
 from datetime import datetime
+import uuid
 
-def create_json_structure(row,eventdate):
+def create_basic_vipe_event(row, header, eventdate):
     """
     Function to create the JSON structure based on the row data from the CSV file.
 
     :param row: A list representing a row from a CSV file.
+    :param header: A list representing the header row of the CSV file.
     :return: A dictionary with the JSON structure based on the CSV row.
-
-
     """
-    
-    import uuid
-
     myuuid = uuid.uuid4()
     
-    return {
+    event = {
         "reference" : str(myuuid),
         "type": row[0],
         "duration": row[1],
@@ -29,13 +26,36 @@ def create_json_structure(row,eventdate):
                 "type": row[6]
             }
         ],
-        "startTime": change_date(row[7],eventdate),
+        "startTime": change_date(row[7], eventdate),
         "behaviors": [
             {
                 "name": row[8]
             }
         ]
     }
+
+    # Add additional JSON if 'gpi' or 'logo' is in the header
+    for i, column_name in enumerate(header):
+        if column_name.lower() == 'gpi' and row[i]:
+            # new dictionary to be added
+            gpi = {
+                "name": "SCTE104_OUT",
+                "params": {
+                    "spliceEventId": row[i],
+                    "breakDuration": "00:05:00:00"
+                },
+                "original": "",
+                "disabled": False,
+                "rule": False
+            }
+
+            # append the new dictionary to the list in 'behaviors'
+            event['behaviors'].append(gpi)
+
+        elif column_name.lower() == 'logo' and row[i]:
+            event['logo'] = row[i]
+
+    return event
 
 
 def change_date(input_datetime, new_date):
@@ -63,54 +83,36 @@ def read_csv_and_create_json(csv_filename, json_filename, eventdate):
     with open(csv_filename, "r") as csv_file:
         # Create a CSV reader
         reader = csv.reader(csv_file)
-        # Skip the header
-        next(reader)
+        # Read the header
+        header = next(reader)
         # Initialize the data
         data = {"events": []}
         # Iterate over the CSV rows
         for row in reader:
-            # Call the create_json_structure function to create the JSON structure
-            event_data = create_json_structure(row, eventdate)
-            print(event_data)
+            print(row[8])
 
-            # Append the event data
-            data["events"].append(event_data)
-        
-        # Write the data to the JSON file
-        with open(json_filename, "w") as json_file:
-            json_file.write(json.dumps(data, indent=4))
+            if row[8] == "CLP":
+                # Call the create_basic_vipe_event function to create the JSON structure
+                event_data = create_basic_vipe_event(row, header, eventdate)
+                # print(event_data)
+
+                # Append the event data
+                data["events"].append(event_data)
+            
+            # Write the data to the JSON file
+            with open(json_filename, "w") as json_file:
+                json_file.write(json.dumps(data, indent=4))
 
 
     print(f"JSON data has been written to '{json_filename}'")
+
 
 if __name__ == "__main__":
 
     SCHEDULE_OUTPUT_DATE = datetime.today().strftime("%Y-%m-%d")
     # Define the CSV and JSON filenames
-    csv_filename = "poc3.csv"
-    json_filename = "data.json"
+    csv_filename = "test_sched.csv"
+    json_filename = "test_sched.json"
 
     # Call the function to read CSV and create JSON
-    read_csv_and_create_json(csv_filename, json_filename,SCHEDULE_OUTPUT_DATE)
-
-    # The below code is currently commented out. 
-    # If you wish to add a new behavior to each event in the JSON, 
-    # and remove the last behavior if there is more than one, you could uncomment and use it.
-
-    # # Load the created JSON file
-    # with open(json_filename, "r") as json_file:
-    #     data = json.load(json_file)
-    
-    # # Add another behavior
-    # extra_behavior = "Behavior2"
-    # for event in data["events"]:
-    #     event["behaviors"].append({"name": extra_behavior})
-
-    # # Remove the last behavior if there is more than one
-    # for event in data["events"]:
-    #     if len(event["behaviors"]) > 1:
-    #         event["behaviors"].pop()
-
-    # # Save the updated JSON data
-    # with open(json_filename, "w") as json_file:
-    #     json_file.write(json.dumps(data, indent=4))
+    read_csv_and_create_json(csv_filename, json_filename, SCHEDULE_OUTPUT_DATE)
